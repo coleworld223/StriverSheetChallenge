50c4ee09081305d93471418af94295b77b7fe99a
coleworld223 <106908583+coleworld223@users.noreply.github.com>
2023-06-10 09:35:54 +0530

    Create maxIncSub(NlogN).cpp

---
+++ DP/maxIncSub(NlogN).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+      int N = nums.size();
+      vector <int> Ans;
+      Ans.push_back(nums[0]);
+
+      for (int i = 1; i < N; i++) {
+        int idx = lower_bound(Ans.begin(), Ans.end(), nums[i]) - Ans.begin();
+        if(idx == Ans.size()) {
+          Ans.push_back(nums[i]);
+        }
+        else{
+          Ans[idx] = nums[i];
+        }
+      }
+      return (int) Ans.size();
+    }
+};
