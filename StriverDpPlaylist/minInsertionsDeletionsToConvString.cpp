737b9188f78487283ff020833f7b1d2312db6e10
coleworld223 <106908583+coleworld223@users.noreply.github.com>
2023-06-18 12:52:25 +0530

    Create minInsertionsDeletionsToConvString.cpp

---
+++ StriverDpPlaylist/minInsertionsDeletionsToConvString.cpp
@@ -0,0 +1,31 @@
+class Solution{
+		
+
+	public:
+	int minOperations(string str1, string str2) 
+	{ 
+	    int n = str1.size();
+	    int m = str2.size();
+	    
+	    vector <vector <int>> DP(n + 1, vector <int> (m + 1, 1e9));
+	    for (int i = 0; i <= n; i++) {
+	        DP[i][0] = i;
+	    }
+	    for (int j = 0; j <= m; j++) {
+	        DP[0][j] = j;
+	    }
+	    
+	    for (int i = 1; i <= n; i++) {
+	        for (int j = 1; j <= m; j++) {
+	            if(str1[i - 1] == str2[j - 1]) {
+	                DP[i][j] = min(DP[i][j], DP[i - 1][j - 1]);
+	            }
+	            else{
+	                DP[i][j] = min({DP[i][j - 1] + 1, DP[i - 1][j] + 1});
+	            }
+	        }
+	    }
+	    return DP[n][m];
+	    
+	} 
+};
